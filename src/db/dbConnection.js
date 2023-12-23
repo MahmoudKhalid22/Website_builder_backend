77e3c51453e901fdb1d13d65643efe397c6f1e7f
MahmoudKhalid <mahmoud0122549@gmail.com>
2023-12-23 23:25:35 +0200

    add database url

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 function connection() {
-  mongoose.connect("mongodb://127.0.0.1:27017/grad-project");
+  mongoose.connect(process.env.MONGODB_URL);
 
   mongoose.connection.once("open", () => console.log("db connected"));
 }
