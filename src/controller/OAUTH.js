ae24d4830abf8d3420078f40d20a73475b0a2655
MahmoudKhalid <mahmoud0122549@gmail.com>
2024-02-03 18:05:22 +0200

    chore: refactor oauth endpoints

--- src/controller/OAUTH.js
+++ src/controller/OAUTH.js
@@ -1,38 +1,45 @@
-import express from 'express';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { Strategy as FacebookStrategy } from 'passport-facebook';
-
-
-
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as FacebookStrategy } from "passport-facebook";
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const FACEBOOK_CLIENT_ID =process.env.FACEBOOK_CLIENT_ID;
+const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID;
 const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET;
 
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: '/google'
-},(accessToken, refreshToken, profile, callback)=>{
-    console.log(profile);
-    callback(null, profile);
-}))
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/google",
+    },
+    (accessToken, refreshToken, profile, callback) => {
+      console.log(profile);
+      callback(null, profile);
+    }
+  )
+);
 
-passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_CLIENT_ID,
-    clientSecret: FACEBOOK_CLIENT_SECRET,
-    callbackURL:'/facebook',
-    profileFields: ['emails', 'displayName', 'name', 'picture']
-}, (accessToken, refreshToken, profile, callback)=>{
-    console.log(profile);
-    callback(null, profile)
-}))
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_CLIENT_ID,
+      clientSecret: FACEBOOK_CLIENT_SECRET,
+      callbackURL: "/facebook",
+      profileFields: ["emails", "displayName", "name", "picture"],
+    },
+    (accessToken, refreshToken, profile, callback) => {
+      console.log(profile);
+      callback(null, profile);
+    }
+  )
+);
 
-passport.serializeUser((user,callback)=>{
-    callback(null, user);
-})
+passport.serializeUser((user, callback) => {
+  callback(null, user);
+});
 
-passport.deserializeUser((user, callback)=>{
-    callback(null, user);
-})
+passport.deserializeUser((user, callback) => {
+  callback(null, user);
+});
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
 dotenv.config();
 import { connection } from "./db/dbConnection.js";
-// import { router as pageRouter } from "./router/pages.js";
 import { userRouter } from "./router/users.js";
 import { pageRouter } from "./router/pages.js";
 import { docs } from "./utils/swagger.js";
-import  passport from "passport";
-// import { oauth } from "./controller/OAUTH.js";
+import "./controller/OAUTH.js";
 
 const app = express();
+
 app.use(express.json());
-import Session from 'express-session';
-
-app.use(Session({
-  secret: process.env.EXPRESS_SESSION_SECRET_KEY,
-  resave: true,
-  saveUninitialized: true
-}))
-
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET_KEY,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(userRouter);
 app.use(pageRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 docs(app);
 connection();
