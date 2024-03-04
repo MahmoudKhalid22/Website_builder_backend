15d4a2d4954975682a1424bcf1268f504469d43f
mahmoudhossam2000 <mahmoudhossam219200@gmail.com>
2024-03-04 21:43:27 +0200

    delete pages

--- src/controller/page.js
+++ src/controller/page.js
@@ -44,7 +44,7 @@ const deletePage = async (req, res) => {
       return res.status(404).json({ error: "Page not found or you are not authorized to delete it." });
     }
 
-    res.json({ message: "Page deleted successfully", deletedPage });
+    res.json({ message: "Page deleted successfully" });
   } catch (err) {
     res.status(500).json({ error: "Internal Server Error" });
   }
