6d6954c218f47e6b63225f5e82b557a3d25cc46b
MahmoudKhalid <mahmoud0122549@gmail.com>
2024-06-30 16:32:50 +0300

    chore: complete user auth, admin, plans unit tests

--- __test__/user.test.js
+++ __test__/user.test.js
@@ -1,13 +1,14 @@
 import request from "supertest";
 import app from "../src/app";
 import { User } from "../src/model/userModel";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import jwt from "jsonwebtoken";
-import sgMail from "@sendgrid/mail";
+import Plan from "../src/model/subPlan";
 
 jest.mock("@sendgrid/mail"); // Mocking the entire module
 
 const userOneId = new mongoose.Types.ObjectId();
+const adminOneId = new mongoose.Types.ObjectId();
 const userOne = {
   _id: userOneId,
   name: "Mahmoud",
@@ -26,9 +27,28 @@ const userOne = {
   ],
 };
 
+const adminOne = {
+  _id: adminOneId,
+  name: "Mahmoud",
+  email: "mahmoudkhalid0122+1@gmail.com",
+  password: "mahmoud444$",
+  role: "admin",
+  verified: true,
+  tokens: [
+    {
+      accessToken: jwt.sign({ _id: adminOneId }, process.env.JWT_SECRET),
+      refreshToken: jwt.sign(
+        { _id: adminOneId },
+        process.env.REFRESH_TOKEN_SECRET_KEY
+      ),
+    },
+  ],
+};
+
 beforeEach(async () => {
   await User.deleteMany();
   await User.create(userOne);
+  await User.create(adminOne);
 });
 
 // TEST CREATE NEW USER
@@ -99,12 +119,23 @@ test("should delete user data unauthorized", async () => {
   await request(app).delete("/user/delete").send().expect(401);
 });
 
-// // 5. UPLOAD USER AVATAR
-// test("should upload user avatar", async () => {
-//   (await request(app).post('/user/upload').set('Authorization', `Bearer ${userOne.tokens[0].token}`)).setEncoding({
+// 5. UPLOAD USER AVATAR
+test("should upload user avatar", async () => {
+  const response = await request(app)
+    .post("/user/upload")
+    .set("Authorization", `Bearer ${userOne.tokens[0].accessToken}`)
+    .attach("avatar", "__test__/fixtures/download.jpeg")
+    .expect(200);
+});
 
-//   });
-// });
+// SHOULD GET USER AVATAR
+test("should get user avatar", async () => {
+  await request(app)
+    .get("/user/avatar")
+    .set("Authorization", "Bearer " + userOne.tokens[0].accessToken)
+    .send()
+    .expect(200);
+});
 
 // 6. SHOULD GET AVATAR
 
@@ -143,9 +174,145 @@ test("should update user name", async () => {
 
 // 11. TEST GET REFRESH TOKEN
 test("should get refresh token", async () => {
-  const response = await request(app)
+  await request(app)
     .get("/user/refresh-token")
     .set("Authorization", `Bearer ${userOne.tokens[0].refreshToken}`)
+    .send()
+    .expect(200);
+});
+
+// 12. TEST ADMIN GET ALL USERS
+test("admin should get all users", async () => {
+  await request(app)
+    .get("/user/admin-users?role=user")
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send()
+    .expect(200);
+});
+
+// 13. TEST GET ALL MESSAGES
+test("admin should get all messages", async () => {
+  await request(app)
+    .get("/message/admin")
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send()
+    .expect(200);
+});
+
+// 14. ADMIN TEST NEW USER
+test("admin should register new user", async () => {
+  await request(app)
+    .post("/user/admin-new-user")
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send({
+      name: "mahmoud from admin",
+      email: "mahmoudkhalid0122+3@gmail.com",
+      password: "pass123$",
+      role: "user",
+      verified: true,
+    })
+    .expect(201);
+});
+
+// 15. TEST GET USER ID PAGES
+test("admin get pages of user", async () => {
+  await request(app)
+    .get(`/user/${userOne._id}/pages`)
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send()
+    .expect(200);
+});
+
+// 16. TEST GET ONE USER PAGE
+
+// 17. TEST DELETE ALL USER'S PAGES
+
+// 18. TEST DELETE ONE USER'S PAGE
+
+// 19. TEST ADMIN BLOCKS USER
+test("admin should block one user", async () => {
+  await request(app)
+    .put(`/user/block/${userOne._id}`)
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send()
+    .expect(200);
+});
+
+// 19. TEST ADMIN UNBLOCKS USER
+test("admin should block one user", async () => {
+  await request(app)
+    .put(`/user/unblock/${userOne._id}`)
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send()
+    .expect(200);
+});
+
+// 19. TEST ADMIN DELETES USER
+test("admin should block one user", async () => {
+  await request(app)
+    .delete(`/user/admin-delete-user/${userOne._id}`)
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send()
+    .expect(200);
+});
+
+// 19. TEST ADMIN SENDS ALERT TO USER
+test("admin should block one user", async () => {
+  await request(app)
+    .post(`/user/send-alert/${userOne._id}`)
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send()
+    .expect(200);
+});
+
+// 20. TEST ADD PLANS
+test("admin should add a new plan", async () => {
+  await request(app)
+    .post("/plan/new")
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send({
+      name: "free plan",
+      price: 0,
+      description: [
+        "you can add a new page",
+        "you can use ai as a helping tool",
+        "you can take your free domain",
+      ],
+    })
+    .expect(201);
+});
+
+// 21. TEST GENERIC USER GETS PLANS
+test("generic user gets all plans", async () => {
+  await request(app).get("/plan").send().expect(200);
+});
+
+// 22. ADMIN UPDATES A PLAN
+test("admin updates an existing plan", async () => {
+  const plans = await Plan.find({});
+  const plan = plans[0];
+  await request(app)
+    .patch("/plan/" + plan._id)
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
+    .send({
+      name: "premium",
+      price: 20,
+      description: [
+        "you can use more advanced ai tools",
+        "you can get your source code",
+        "you can get paid template",
+        "you can customize your domain",
+      ],
+    })
+    .expect(200);
+});
+// 22. ADMIN DELETES A PLAN
+test("admin updates an existing plan", async () => {
+  const plans = await Plan.find({});
+  const plan = plans[0];
+  await request(app)
+    .delete("/plan/" + plan._id)
+    .set("Authorization", `Bearer ${adminOne.tokens[0].accessToken}`)
     .send()
     .expect(200);
 });
