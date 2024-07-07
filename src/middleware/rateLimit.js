5a4f0d0c2e2c889dd60bf7ff6465c9c127b5218f
mahmoudhossam2000 <mahmoudhossam219200@gmail.com>
2024-07-07 18:22:16 +0300

    rate limit update

--- src/middleware/rateLimit.js
+++ src/middleware/rateLimit.js
@@ -1,17 +1,17 @@
-// rateLimit.js
-
 import rateLimit from "express-rate-limit";
 import rawBody from "raw-body";
 
-// Rate limit setup: 100 requests per hour per raw body
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour window
   max: 100, // limit each raw body to 100 requests per windowMs
   message: "Too many requests with this raw body, please try again later.",
   keyGenerator: async (req) => {
-    // Use raw body as the key for rate limiting
     try {
-      const body = await rawBody(req);
+      const body = await rawBody(req, {
+        length: req.headers["content-length"],
+        limit: "1mb", // Adjust the limit based on your needs
+        encoding: "utf8",
+      });
       return body.toString();
     } catch (err) {
       console.error("Error reading raw body:", err);
